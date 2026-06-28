9138bde328633f6f7dd7584129936481b2540c1c
sarthu210 <sarthu102@gmail.com>
2026-06-29 00:05:28 +0530

    Zod validation added and also created test case

--- server/config/db/connectDB.js
+++ server/config/db/connectDB.js
@@ -16,7 +16,6 @@ async function connectDB() {
     }
     catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
-        process.exit(1); // Exit process with failure
     }
 }
 
