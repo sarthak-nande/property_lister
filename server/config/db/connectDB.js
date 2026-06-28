5870f5f2005fc08986fa1113acc68d749121d842
sarthu210 <sarthu102@gmail.com>
2026-06-28 15:51:04 +0530

    Db connection done

---
+++ server/config/db/connectDB.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+async function connectDB() {
+    try {
+        const url = process.env.MONGO_URI;
+
+        if(!url) {
+            throw new Error('MONGO_URI is not defined in environment variables');
+        }
+        
+        const connect = await mongoose.connect(url);
+
+        console.log(`MongoDB Connected: ${connect.connection.host}`);
+
+    }
+    catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+    }
+}
+
+export default connectDB;
